12b685da42d5a88adb6a0d07ce1c888870c1a582
Dmytro Hushchyk <107995555+DHushchyk@users.noreply.github.com>
2022-10-20 19:30:23 +0300

    Updated max length of redirect link

--- shortener/models.py
+++ shortener/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 class Shortener(models.Model):
     original_url = models.URLField(unique=True)
     short_url = models.CharField(max_length=6, unique=True, blank=True)
-    redirect_link = models.CharField(max_length=38, unique=True, blank=True)
+    redirect_link = models.CharField(max_length=255, unique=True, blank=True)
     last_redirect_time = models.DateTimeField(null=True)
     redirect_count = models.IntegerField(default=0)
     is_deleted = models.BooleanField(default=False)
